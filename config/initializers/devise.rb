15d9ef96f7cc36eb44cc156adc2cfc864e722c3f
Amr Fathy <amruafathy@gmail.com>
2020-02-07 20:24:53 +0100

    Add some debugging links

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,6 +262,8 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, '2550877271676365', '8039f24f3bdb761e2fa503d734cc2b94', scope: 'public_profile, email',
                   callback_url: 'http://localhost:3000/customers/auth/facebook/callback'
+                  # callback_url: 'https://macc-store-app.herokuapp.com/customers/auth/facebook/callback'
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
